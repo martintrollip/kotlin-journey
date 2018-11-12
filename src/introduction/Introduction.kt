6c913faf2a91b68fb8fdeed2905bff51ea97d68c
Martin Trollip <martint@discovery.co.za>
2018-11-12 21:22:00 +0200

    [koans][feature]Introduction, hello world, named arguments, default arguments, lamdas, strings

--- src/introduction/Introduction.kt
+++ src/introduction/Introduction.kt
@@ -5,10 +5,103 @@ package introduction
  * @since 2018/11/12 20:38
  */
 fun main(args: Array<String>) {
+    //Hello world!
     System.out.println(start())
+
+    //Name arguments
+    System.out.println(namedArguements(mutableListOf("a", "b", "c")))
+
+    //Default arguments
+    System.out.println(foo("a"))
+    System.out.println(foo("b", 1))
+    System.out.println(foo("c", toUpperCase = true))
+    System.out.println(foo("d", 2, true))
+
+    //Lamdas
+    System.out.println("containsEven: " + containsEven(mutableListOf(1,3,5,7)))
+    System.out.println("containsEven: " + containsEven(mutableListOf(1,2,5,7)))
+
+    //Strings
+    System.out.println("date match: 13 APR 1992: " + "13 APR 1992".matches(getPattern().toRegex()))
+    System.out.println("date match: 12 MAT 1992: " + "12 MAT 1992".matches(getPattern().toRegex()))
+    System.out.println("date match: 14 DEC 2018: " + "14 DEC 2018".matches(getPattern().toRegex()))
+    System.out.println("date match: 14 DEC 18: " + "14 DEC 18".matches(getPattern().toRegex()))
 }
 
 /**
- * Take a look at function syntax and make the function start return the string "OK".
+ * Take a look at <a href="http://kotlinlang.org/docs/reference/basic-syntax.html#defining-functions">function syntax</a> and make the function start return the string "OK".
  */
 fun start(): String = "OK"
+
+/**
+ * <a href="kotlinlang.org/docs/reference/functions.html#default-arguments">Default and named arguments</a> help to minimize the number of overloads and improve the readability of the function invocation. The library function joinToString is declared with default values for parameters:
+ *
+ * <code>
+ *      fun joinToString(
+ *      separator: String = ", ",
+ *      prefix: String = "",
+ *      postfix: String = "",
+ *      /* ... */
+ *      ): String
+ * </code>
+ *
+ * It can be called on a collection of Strings. Specifying only two arguments make the function joinOptions() return the list in a JSON format (e.g., "[a, b, c]")
+ */
+fun namedArguements(options: Collection<String>): String {
+    return options.joinToString(prefix = "[", postfix = "]")
+}
+
+/**
+ * Default arguments
+ *
+ * There are several overloads of 'foo()' in Java:
+ *
+ * <code>
+ *      public String foo(String name, int number, boolean toUpperCase) {
+ *          return (toUpperCase ? name.toUpperCase() : name) + number;
+ *      }
+ *
+ *      public String foo(String name, int number) {
+ *          return foo(name, number, false);
+ *      }
+ *
+ *       public String foo(String name, boolean toUpperCase) {
+ *          return foo(name, 42, toUpperCase);
+ *      }
+ *
+ *      public String foo(String name) {
+ *          return foo(name, 42);
+ *      }
+ * </code>
+ *
+ * All these Java overloads can be replaced with one function in Kotlin. Change the declaration of the function foo in a way that makes the code using foo compile.
+ * Use <a href="http://kotlinlang.org/docs/reference/functions.html#default-arguments">default and named arguments</a>.
+ */
+fun foo(name: String, number: Int = 42, toUpperCase: Boolean = false) =
+        (if (toUpperCase) name.toUpperCase() else name) + number
+
+/**
+ * Kotlin supports a functional style of programming. Read about <a href="http://kotlinlang.org/docs/reference/lambdas.html">higher-order functions and function literals (lambdas)</a> in Kotlin.
+ * Pass a lambda to any function to check if the collection contains an even number. The function any gets a predicate as an argument and returns true if there is at least one element satisfying the predicate.
+ */
+fun containsEven(collection: Collection<Int>): Boolean {
+    return collection.any({
+        it % 2 == 0
+    })
+}
+
+/**
+ * Read about <a href="http://kotlinlang.org/docs/reference/basic-types.html#string-literals">different string literals and string templates</a> in Kotlin.
+ *
+ * Raw strings are useful for writing regex patterns, you don't need to escape a backslash by a backslash. Below there is a pattern that matches a date in format 13.06.1992 (two digits, a dot, two digits, a dot, four digits):
+ *
+ *
+ * <code>
+ *    fun getPattern() = """\d{2}\.\d{2}\.\d{4}"""
+ * </code>
+ *
+ * Using month variable rewrite this pattern in such a way that it matches the date in format 13 JUN 1992 (two digits, a whitespace, a month abbreviation, a whitespace, four digits).
+ */
+var month = "(JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)"
+
+fun getPattern(): String = """\d{2} $month \d{4}"""
