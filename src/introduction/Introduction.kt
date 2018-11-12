83b5403e041573dba3f35a1b93d5848485a25289
Martin Trollip <martint@discovery.co.za>
2018-11-12 20:42:47 +0200

    [koans][feature]Hello world (print ok)

---
+++ src/introduction/Introduction.kt
@@ -0,0 +1,14 @@
+package introduction
+
+/**
+ * @author Martin Trollip <martint@discovery.co.za>
+ * @since 2018/11/12 20:38
+ */
+fun main(args: Array<String>) {
+    System.out.println(start())
+}
+
+/**
+ * Take a look at function syntax and make the function start return the string "OK".
+ */
+fun start(): String = "OK"
