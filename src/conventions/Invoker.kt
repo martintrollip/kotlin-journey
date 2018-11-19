fd7ebd84d4db57df2c9956d1235455f13f859e27
Martin Trollip <martint@discovery.co.za>
2018-11-19 19:34:28 +0200

    [koans][feature]Conventions, operator overloading, destructing constructors, invokers

---
+++ src/conventions/Invoker.kt
@@ -0,0 +1,16 @@
+package conventions
+
+/**
+ * @author Martin Trollip
+ * @since 2018/11/19 19:27
+ */
+class Invoker {
+    var numberOfInvocations: Int = 0
+        private set
+
+    operator fun invoke(): Invoker {
+        numberOfInvocations++
+        println("Invoked $numberOfInvocations")
+        return this
+    }
+}
