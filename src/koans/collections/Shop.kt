28138d36dd06c3bfac6016cd9ec32bcad7b5dd92
Martin Trollip <martint@discovery.co.za>
2018-12-07 18:36:45 +0200

    [koans][feature]Finishing collections

--- src/koans/collections/Shop.kt
+++ src/koans/collections/Shop.kt
@@ -251,11 +251,11 @@ fun Customer.getOrderedProductsList(): List<Product> {
  * </code>
  *
  * */
-//fun doSomethingStrangeWithCollection(collection: Collection<String>): Collection<String>? {
-//
-//    val groupsByLength = collection. groupBy { s -> TODO() }
-//
-//    val maximumSizeOfGroup = groupsByLength.values.map { group -> TODO() }.max()
-//
-//    return groupsByLength.values.firstOrNull { group -> TODO() }
-//}
+fun doSomethingStrangeWithCollection(collection: Collection<String>): Collection<String>? {
+
+    val groupsByLength = collection. groupBy { s -> s.length }
+
+    val maximumSizeOfGroup = groupsByLength.values.map { group -> group.size }.max()
+
+    return groupsByLength.values.firstOrNull { group -> group.size == maximumSizeOfGroup }
+}
