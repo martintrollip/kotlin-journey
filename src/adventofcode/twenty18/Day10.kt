1ab025d69618350e7944af8467ff0084bc562f80
Martin Trollip <martint@discovery.co.za>
2018-12-11 16:35:49 +0200

    [adventofcode][feature]Day 9, part 2

--- src/adventofcode/twenty18/Day10.kt
+++ src/adventofcode/twenty18/Day10.kt
@@ -1,8 +1,7 @@
 package adventofcode.twenty18
 
 import java.io.File
-import java.lang.Math.pow
-import kotlin.math.max
+import kotlin.math.absoluteValue
 import kotlin.math.sqrt
 
 /**
@@ -10,10 +9,12 @@ import kotlin.math.sqrt
  * @since 2018/12/10 06:51
  */
 val POINT_OF_LIGHT_REGEX = "position=<\\s?(\\-?[0-9]+), \\s?(\\-?[0-9]+)> velocity=<\\s?(\\-?[0-9]+), \\s?(\\-?[0-9]+)>".toRegex()
-val MAX_RUNS = 10000000
+val MAX_RUNS = 1000000000
 var LETTER_HEIGHT = 8//Assuming the letters has the same height as example
 
 const val DAY10_INPUT = "src/res/day10_input_small"
+
+//303710075
 
 fun main(args: Array<String>) {
     var count = 0
@@ -21,33 +22,37 @@ fun main(args: Array<String>) {
     val lightPoints = File(DAY10_INPUT).readLines().map {
         val matchResult = POINT_OF_LIGHT_REGEX.find(it)
         val (x, y, dx, dy) = matchResult!!.destructured
-        LightPoint(count++, x.toInt(), y.trim().toInt(), dx.toInt(), dy.toInt())
+        LightPoint(count++, x.toLong(), y.trim().toLong(), dx.toLong(), dy.toLong())
     }
+
+
 
     simulate(lightPoints)
 }
 
 fun simulate(points: List<LightPoint>) {
-    var detected = false
+    var textDetected = false
     var count = -1
-    while (!detected && count < MAX_RUNS) {
+
+    while (!textDetected && count < MAX_RUNS) {
         count++
         points.move()
-        detected = letterDetector(points)
+        textDetected = letterDetector(points)
     }
 
-    println("Possible detection at ${count + 1}")
-//    points.normalise()
-    val sorted = points.sortedWith(compareBy({ it.x }, { it.y }))
-    buildArray(sorted).print()
+    println("Possible detection at $count")
+    println(buildArray(points).print())
 }
 
 fun letterDetector(points: List<LightPoint>): Boolean {
-    val yMax = points.maxBy { it.y }?.y
-    val yMin = points.minBy { it.y }?.y
+    val yMin = points.minBy { it.y }?.y!!
+    val yMax = points.maxBy { it.y }?.y!!
+    val height = distance(yMax, yMin)
 
-    return sqrt(pow(yMax?.toDouble()!!, 2.0) + pow(yMin?.toDouble()!!, 2.0)) <= LETTER_HEIGHT //Assuming 1 line
+    return height <= LETTER_HEIGHT //Assuming 1 line
 }
+
+private fun distance(a: Long, b: Long) = sqrt(((a * a) + (b * b)).toDouble()).toInt() + 1
 
 fun List<LightPoint>.move() {
     for (lightPoint in this) {
@@ -56,37 +61,50 @@ fun List<LightPoint>.move() {
     }
 }
 
+fun List<LightPoint>.move(steps: Int) {
+    for (lightPoint in this) {
+        lightPoint.x += steps * lightPoint.dx
+        lightPoint.y += steps * lightPoint.dy
+    }
+}
+
 fun List<LightPoint>.normalise() {
     val xOffset = minBy { it.x }?.x
     val yOffset = minBy { it.y }?.y
 
     forEach {
-        it.x -= xOffset!!
-        it.y -= yOffset!!
+        it.x -= xOffset?.absoluteValue!!
+        it.y -= yOffset?.absoluteValue!!
     }
 }
 
 fun buildArray(points: List<LightPoint>): Array<Array<LightPoint>> {
-    val height = points.maxBy { it.y }?.y!! + 10
-    val width = points.maxBy { it.x }?.x!! + 10
-    val size = max(height, width)
+    points.normalise()
+    val sorted = points.sortedWith(compareBy({ it.x }, { it.y }))
 
-    val array = Array(size, { Array(size, { LightPoint() }) })
-    for (point in points) {
-        if (point.x >= 0 && point.y >= 0 && point.x < size && point.y < size) {
-            array[point.x][point.y] = point
+    val xMax = sorted.maxBy { it.x }?.x!!
+    val xMin = sorted.minBy { it.x }?.x!!
+    val yMax = sorted.maxBy { it.y }?.y!!
+    val yMin = sorted.minBy { it.y }?.y!!
+    val width = distance(xMin, xMax)
+    val height = distance(yMin, yMax)
+
+    val array = Array(height, { Array(width, { LightPoint() }) })
+    for (point in sorted) {
+        if (point.x >= 0 && point.y >= 0 && point.x <= width && point.y <= height) {
+            array[point.y.toInt()][point.x.toInt()] = point // :/
         }
     }
 
     return array
 }
 
-fun Array<Array<LightPoint>>.print() {
+fun Array<Array<LightPoint>>.print(): String {
     var print = ""
     for (row in 0 until this.size) {
         var line = ""
         for (column in 0 until this[row].size) {
-            line += if (this[column][row].id >= 0) {
+            line += if (this[row][column].id >= 0) {
                 "#"
             } else {
                 "."
@@ -94,8 +112,9 @@ fun Array<Array<LightPoint>>.print() {
         }
         print += "$line\n"
     }
-    println(print)
+    return print
+//    println(print)
 //    File("$iteration.txt").writeText(print)
 }
 
-data class LightPoint(var id: Int = -1, var x: Int = -1, var y: Int = -1, var dx: Int = -1, var dy: Int = -1)
+data class LightPoint(var id: Int = -1, var x: Long = -1, var y: Long = -1, var dx: Long = -1, var dy: Long = -1)
