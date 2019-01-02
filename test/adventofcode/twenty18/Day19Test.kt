ca70dbdddb4ab3ef53ed5108f451aa7da1cc3374
Martin Trollip <martint@discovery.co.za>
2019-01-02 18:44:18 +0200

    [adventofcode][feature]Day 19 part 2

--- test/adventofcode/twenty18/Day19Test.kt
+++ test/adventofcode/twenty18/Day19Test.kt
@@ -3,6 +3,7 @@ package adventofcode.twenty18
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
+import kotlin.system.measureTimeMillis
 
 /**
  * @author Martin Trollip <martint@discovery.co.za>
@@ -11,10 +12,10 @@ import org.junit.Test
 class Day19Test {
 
     var DAY19_INPUT_SMALL = "src/res/day19_input_small"
+    var DAY19_INPUT = "src/res/day19_input"
 
     var pointer: Pointer? = null
-    var sample: ArrayList<NamedInstruction>? = null
-    var newRegister = listOf(Register("0"), Register("1"), Register("2"), Register("3"), Register("4"), Register("5"))
+    var sample: Map<Int, NamedInstruction>? = null
 
     @Before
     fun setup() {
@@ -32,13 +33,13 @@ class Day19Test {
         assertEquals(0, pointer!!.value)
 
         assertTrue(sample?.get(0) is NamedInstruction)
-        assertEquals("seti", (sample?.get(0) as NamedInstruction).opCodeName)
+        assertEquals("seti", (sample?.get(0) as NamedInstruction).opcode.name)
         assertEquals(5, (sample?.get(0) as NamedInstruction).a)
         assertEquals(0, (sample?.get(0) as NamedInstruction).b)
         assertEquals(1, (sample?.get(0) as NamedInstruction).c)
 
         assertTrue(sample?.get(4) is NamedInstruction)
-        assertEquals("addr", (sample?.get(3) as NamedInstruction).opCodeName)
+        assertEquals("addr", (sample?.get(3) as NamedInstruction).opcode.name)
         assertEquals(1, (sample?.get(3) as NamedInstruction).a)
         assertEquals(2, (sample?.get(3) as NamedInstruction).b)
         assertEquals(3, (sample?.get(3) as NamedInstruction).c)
@@ -54,5 +55,30 @@ class Day19Test {
         assertEquals(0, execute[3].value)
         assertEquals(0, execute[4].value)
         assertEquals(9, execute[5].value)
+    }
+
+    @Test
+    fun testPart1() {
+        println(
+                measureTimeMillis {
+                    val part1 = part1(readDay19Pointer(DAY19_INPUT), readDay19Input(DAY19_INPUT))
+                    assertEquals(984, part1)
+                }.toString() + "ms")
+        //old time 7172ms
+        //new time 5714ms (removed for loop, let all operations modify the same instance of the register)
+        //newest time 404ms (toString is expensive!)
+        //newest time 318ms (get only once registers[pointer.address])
+        //newest time 307ms (use int indexes on map)
+        //newest time 292ms (move pointer register assignment out of for loop)
+        //newest time 230ms (assign operations beforehand)
+    }
+
+    @Test
+    fun testPart2() {
+        println(
+                measureTimeMillis {
+                    val part1 = part2(readDay19Pointer(DAY19_INPUT), readDay19Input(DAY19_INPUT))
+                    assertEquals(984, part1)
+                }.toString() + "ms")
     }
 }
