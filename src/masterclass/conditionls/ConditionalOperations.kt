c3cd920dbbd01dd97b6c6697b25a9b236ebfe4c4
Martin Trollip <martint@discovery.co.za>
2019-02-17 17:18:14 +0200

    [udemy-kadm][feature]Conditions, operations and iteration

---
+++ src/masterclass/conditionls/ConditionalOperations.kt
@@ -0,0 +1,18 @@
+package masterclass.conditionls
+
+/**
+ * @author Martin Trollip ***REMOVED***
+ * @since 2019/02/17 16:43
+ */
+fun main(args: Array<String>) {
+   /* > <  == != <= >= */
+    val a = 10
+    val b = 20
+
+    println("$a > $b ${a > b}")
+    println("$a < $b ${a < b}")
+    println("$a == $b ${a == b}")
+    println("$a != $b ${a != b}")
+    println("$a <= $b ${a <= b}")
+    println("$a >= $b ${a >= b}")
+}
