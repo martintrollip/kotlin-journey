8b8b5e2fdf6884c3573c8b81fd417b73d67a3586
Martin Trollip <martint@discovery.co.za>
2019-06-08 20:50:46 +0200

    [kadm][feature] Section 15 - Recycler views

--- masterclass-app/app/src/main/java/app/masterclass/kotlinmasterclassapplication/AnotherActivity.kt
+++ masterclass-app/app/src/main/java/app/masterclass/kotlinmasterclassapplication/AnotherActivity.kt
@@ -2,14 +2,33 @@ package app.masterclass.kotlinmasterclassapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import app.masterclass.kotlinmasterclassapplication.data.PersonAdapter
+import app.masterclass.kotlinmasterclassapplication.model.Person
 import kotlinx.android.synthetic.main.activity_another.*
 
 class AnotherActivity : AppCompatActivity() {
+
+    private var adapter: PersonAdapter? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_another)
         val data = intent.extras
-        txtName.text = data?.get("Name").toString()
+
+        val personList = ArrayList<Person>()
+        val layoutManager = LinearLayoutManager(this)
+
+        var adapter = PersonAdapter(personList)
+
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = adapter
+
+        for (i in 0 until 10){
+            personList.add(Person("Name $i", "Number $i", 0))
+        }
+
+        adapter.notifyDataSetChanged()
     }
 }
