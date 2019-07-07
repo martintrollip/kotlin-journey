361c12885cd714860832bdb8be6475ab2369474c
Martin Trollip <martint@discovery.co.za>
2019-07-07 19:01:19 +0200

    [kadm][feature] Section 18 - Part A - Setting up Firebase and basic hardcoded auth

--- masterclass-app/app/src/main/java/app/masterclass/kotlinmasterclassapplication/model/Person.kt
+++ masterclass-app/app/src/main/java/app/masterclass/kotlinmasterclassapplication/model/Person.kt
@@ -6,4 +6,4 @@ import android.support.annotation.DrawableRes
  * @author Martin Trollip <martint@discovery.co.za>
  * @since 2019/06/03 19:02
  */
-data class Person(var name: String?, var number: String?, @DrawableRes var picture: Int)
+data class Person(var name: String = "", var number: String = "", @DrawableRes var picture: Int = 0, var age: Int = 0)
