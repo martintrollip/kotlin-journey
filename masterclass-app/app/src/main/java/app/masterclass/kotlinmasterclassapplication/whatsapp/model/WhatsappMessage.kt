d1cfaaa815d5e2d18a73e641a2b70b336994cf5d
Martin Trollip <martint@discovery.co.za>
2019-07-23 09:31:52 +0200

    [kadm][feature] Section 18 - Whatsapp clone, implemented ChatActivity

---
+++ masterclass-app/app/src/main/java/app/masterclass/kotlinmasterclassapplication/whatsapp/model/WhatsappMessage.kt
@@ -0,0 +1,7 @@
+package app.masterclass.kotlinmasterclassapplication.whatsapp.model
+
+/**
+ * @author Martin Trollip
+ * @since 2019/07/22 09:54
+ */
+data class WhatsappMessage(var id: String = "", var text: String = "", var name: String = "")
