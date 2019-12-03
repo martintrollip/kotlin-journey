198b618a12e2f0baae58f095431307242d46ad63
Martin Trollip <martint@discovery.co.za>
2019-12-03 06:36:45 +0200

    [adventofcode] 2018 Started day 15

---
+++ src/adventofcode/twenty18/Day15.kt
@@ -0,0 +1,16 @@
+package adventofcode.twenty18
+
+/**
+ * @author Martin Trollip
+ * @since 2019/12/02 20:15
+ */
+
+private const val DAY15_INPUT = "src/res/day15_input"
+
+fun main(args: Array<String>) {
+    val day15 = Day15(DAY15_INPUT)
+}
+
+class Day15(val input: String) {
+
+}
---
+++ test/adventofcode/twenty18/Day15Test.kt
@@ -0,0 +1,10 @@
+package adventofcode.twenty18
+
+/**
+ * @author Martin Trollip ***REMOVED***
+ * @since 2019/12/02 20:17
+ */
+class Day15Test {
+
+    private val DAY15_INPUT_SMALL = "src/res/day15_input_small"
+}
