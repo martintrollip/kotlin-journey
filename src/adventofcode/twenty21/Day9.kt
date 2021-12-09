edc81a40001d9792a41ae999179007a3aea5d7fc
Martin Trollip <Martin.Trollip@hensoldt.net>
2021-12-09 09:15:29 +0200

    [adventofcode] 2021 Day 9 part 1 and 2

--- src/adventofcode/twenty21/Day9.kt
+++ src/adventofcode/twenty21/Day9.kt
@@ -1,33 +1,103 @@
 package adventofcode.twenty21
 
+import java.awt.Point
 import java.io.File
 
 /**
  * @author Martin Trollip
  * @since 2021/12/08 14:30
  */
-private const val DAY8_INPUT = "src/res/twenty21/day8_input"
+private const val DAY9_INPUT = "src/res/twenty21/day9_input"
 
 fun main() {
     val day9 = Day9()
     println(
-//        "In the output values, how many times do digits 1, 4, 7, or 8 appear? ${
-//            day8.part1(day8.readInput(DAY8_INPUT))
-//        }"
+        "What is the sum of the risk levels of all low points on your heightmap? ${
+            day9.part1(day9.readInput(DAY9_INPUT))
+        }"
     )
     println(
-//        "What do you get if you add up all of the output values? ${
-//            day8.part2(day8.readInput(DAY8_INPUT))
-//        }"
+        "What do you get if you multiply together the sizes of the three largest basins? ${
+            day9.part2(day9.readInput(DAY9_INPUT))
+        }"
     )
 }
 
 class Day9 {
 
-    fun readInput(fileName: String): List<String> {
-        return File(fileName).readLines()
+    fun readInput(fileName: String): Map<Point, Int> {
+        var row = 0
+        var col = 0
+        val map = mutableMapOf<Point, Int>()
+
+        File(fileName).readLines().forEach {
+            it.forEach {
+                map[Point(col, row)] = it.toString().toInt()
+                col++
+            }
+            row++
+            col = 0
+        }
+        return map
     }
 
+    fun part1(input: Map<Point, Int>): Int {
+        val lowPoints = calculateLowPoints(input)
+        return lowPoints.map { it.value }.sumOf { it + 1 }
+    }
 
+    fun part2(input: Map<Point, Int>): Int {
+        val basins = calculateBasins(input)
+        return basins.map { it.size }.sortedDescending().take(3).reduce { acc, it -> acc * it }
+    }
+
+    /**
+     * Return the points where all neighbours are greater than the current point.
+     */
+    private fun calculateLowPoints(input: Map<Point, Int>): Map<Point, Int> {
+        val lowPoints = mutableMapOf<Point, Int>()
+        input.forEach { (point, value) ->
+            val neighbours = input.filter { isNeighbour(it, point) }
+            if (neighbours.values.all { it > value }) {
+                lowPoints[point] = value
+            }
+        }
+        return lowPoints
+    }
+
+    private fun isNeighbour(it: Map.Entry<Point, Int>, point: Point) =
+        ((it.key.x in (point.x - 1)..(point.x + 1) && it.key.y in (point.y - 1)..(point.y + 1))
+                && (it.key.x == point.x || it.key.y == point.y) && !(it.key.x == point.x && it.key.y == point.y))
+
+    /**
+     * Remove points with a value of 9 and group the remaining points if they are adjacent.
+     */
+    private fun calculateBasins(input: Map<Point, Int>): List<List<Point>> {
+        val basins = mutableListOf<List<Point>>()
+        val lowPoints = calculateLowPoints(input)
+
+        lowPoints.forEach { (point, value) ->
+            run {
+                val basin = mutableListOf(point)
+                var neighbours = findNeighbours(input, listOf(point))
+
+                while (neighbours.isNotEmpty()) {
+                    basin.addAll(neighbours)
+                    neighbours = findNeighbours(input, neighbours).filter { !basin.contains(it) }
+                }
+                basins.add(basin)
+            }
+        }
+        return basins
+    }
+    
+    private fun findNeighbours(input: Map<Point, Int>, points: List<Point>): List<Point> {
+        val neighbours = mutableListOf<Point>()
+        points.forEach { point ->
+            val neighbour = input.filter { isNeighbour(it, point) && it.value != 9 }.map { it.key }
+            neighbours.addAll(neighbour.filter { !neighbours.contains(it) })
+        }
+        return neighbours
+    }
 }
 
--- test/adventofcode/twenty21/Day9Test.kt
+++ test/adventofcode/twenty21/Day9Test.kt
@@ -10,5 +10,39 @@ import org.junit.Test
 class Day9Test {
     private val DAY9_INPUT_EXAMPLE = "src/res/twenty21/day9_input_example"
     private val DAY9_INPUT = "src/res/twenty21/day9_input"
+
+    /**
+     * Your first goal is to find the low points - the locations that are lower than any of its adjacent locations. Most locations have four adjacent locations (up, down, left, and right); locations on the edge or corner of the map have three or two adjacent locations, respectively. (Diagonal locations do not count as adjacent.)
+     *
+     * In the above example, there are four low points, all highlighted: two are in the first row (a 1 and a 0), one is in the third row (a 5), and one is in the bottom row (also a 5). All other locations on the heightmap have some lower adjacent location, and so are not low points.
+     *
+     * The risk level of a low point is 1 plus its height. In the above example, the risk levels of the low points are 2, 1, 6, and 6. The sum of the risk levels of all low points in the heightmap is therefore 15.
+     */
+    @Test
+    fun day9_calculateLowPoints_example() {
+        val day9 = Day9()
+        val result = day9.part1(day9.readInput(DAY9_INPUT_EXAMPLE))
+        assertEquals(15, result)
+    }
     
+    @Test
+    fun day9_calculateLowPoints_withInput() {
+        val day9 = Day9()
+        val result = day9.part1(day9.readInput(DAY9_INPUT))
+        assertEquals(436, result)
+    }
+
+    @Test
+    fun day9_calculateBasins_example() {
+        val day9 = Day9()
+        val result = day9.part2(day9.readInput(DAY9_INPUT_EXAMPLE))
+        assertEquals(1134, result)
+    }
+
+    @Test
+    fun day9_calculateBasins_withInput() {
+        val day9 = Day9()
+        val result = day9.part2(day9.readInput(DAY9_INPUT))
+        assertEquals(436, result)
+    }
 }
