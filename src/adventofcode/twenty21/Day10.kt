b4b89d046c18e8a1e0007eb4c8ceeb549ee2f5f5
Martin Trollip <Martin.Trollip@hensoldt.net>
2021-12-10 08:30:04 +0200

    [adventofcode] 2021 Day 10 part 1 and 2

---
+++ src/adventofcode/twenty21/Day10.kt
@@ -0,0 +1,127 @@
+package adventofcode.twenty21
+
+import java.io.File
+import java.util.*
+
+/**
+ * @author Martin Trollip
+ * @since 2021/12/10 07:06
+ */
+private const val DAY10_INPUT = "src/res/twenty21/day10_input"
+
+fun main() {
+    val day9 = Day9()
+    println(
+        "What is the total syntax error score for those errors?? ${
+            day9.part1(day9.readInput(DAY10_INPUT))
+        }"
+    )
+//    println(
+//        "What is the middle score? ${
+//            day9.part2(day9.readInput(DAY9_INPUT))
+//        }"
+//    )
+}
+
+class Day10 {
+
+    fun readInput(fileName: String): List<String> {
+        return File(fileName).readLines()
+    }
+
+    fun part1(input: List<String>): Int {
+        return calculateSyntaxErrorScore(input)
+    }
+
+    fun part2(input: List<String>): Long {
+        val completedLines = input.filter { isIncomplete(it) }.map { completeIncompleteStack(it) }
+        return completedLines.map { calculateAutoCompleteScore(it) }.sorted()[completedLines.size / 2]
+    }
+
+    private fun calculateSyntaxErrorScore(input: List<String>): Int {
+        return input.sumOf { calculateSyntaxErrorScore(it) }
+    }
+
+    private fun calculateSyntaxErrorScore(line: String): Int {
+        val lineStack = buildStack(line)
+        return when (lineStack.illegalChar()) {
+            ')' -> 3
+            ']' -> 57
+            '}' -> 1197
+            '>' -> 25137
+            else -> 0
+        }
+    }
+
+    private fun calculateAutoCompleteScore(line: List<Char>): Long {
+        var score = 0L
+
+        line.forEach { char ->
+            score *= 5
+            when (char) {
+                ')' -> score += 1
+                ']' -> score += 2
+                '}' -> score += 3
+                '>' -> score += 4
+            }
+        }
+
+        return score
+    }
+
+    private fun isIncomplete(line: String): Boolean {
+        return buildStack(line).containsOnlyOpenBrackets()
+    }
+
+    private fun completeIncompleteStack(line: String): List<Char> {
+        return buildStack(line).map { it.invertOpenBracket() }.reversed()
+    }
+
+    private fun buildStack(line: String): Stack<Char> {
+        val stack = Stack<Char>()
+
+        for (c in line) {
+            if (!c.isBracket())
+                continue
+
+            if (stack.isNotEmpty() && c.matchesBracket(stack.peek())) {
+                stack.pop()
+            } else {
+                stack.push(c)
+            }
+        }
+
+        return stack
+    }
+
+    private fun Char.isBracket(): Boolean {
+        return this == '[' || this == ']' || this == '{' || this == '}' || this == '(' || this == ')' || this == '<' || this == '>'
+    }
+
+    private fun Char.isOpenBracket(): Boolean {
+        return this == '[' || this == '{' || this == '(' || this == '<'
+    }
+
+    private fun Char.matchesBracket(other: Char): Boolean {
+        return this == ']' && other == '[' || this == '}' && other == '{' || this == ')' && other == '(' || this == '>' && other == '<'
+    }
+
+    private fun Char.invertOpenBracket(): Char {
+        return when (this) {
+            '[' -> ']'
+            '{' -> '}'
+            '(' -> ')'
+            '<' -> '>'
+            else -> throw IllegalArgumentException("$this is not a open bracket")
+        }
+    }
+
+    private fun Stack<Char>.containsOnlyOpenBrackets(): Boolean {
+        return this.isEmpty() || this.all { it.isOpenBracket() }
+    }
+
+    private fun Stack<Char>.illegalChar(): Char? {
+        return this.firstOrNull { !it.isOpenBracket() }
+    }
+}
+
---
+++ test/adventofcode/twenty21/Day10Test.kt
@@ -0,0 +1,62 @@
+package adventofcode.twenty21
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * @author Martin Trollip
+: @since 2021/12/10 07:08
+ */
+class Day10Test {
+    private val DAY10_INPUT_EXAMPLE = "src/res/twenty21/day10_input_example"
+    private val DAY10_INPUT = "src/res/twenty21/day10_input"
+
+    /**
+     * Did you know that syntax checkers actually have contests to see who can get the high score for syntax errors in a file? It's true! To calculate the syntax error score for a line, take the first illegal character on the line and look it up in the following table:
+     *
+     * ): 3 points.
+     * ]: 57 points.
+     * }: 1197 points.
+     * >: 25137 points.
+     * In the above example, an illegal ) was found twice (2*3 = 6 points), an illegal ] was found once (57 points), an illegal } was found once (1197 points), and an illegal > was found once (25137 points). So, the total syntax error score for this file is 6+57+1197+25137 = 26397 points!/
+     *
+     */
+    @Test
+    fun day10_calculateScore_example() {
+        val day10 = Day10()
+        val result = day10.part1(day10.readInput(DAY10_INPUT_EXAMPLE))
+        assertEquals(26397, result)
+    }
+
+    @Test
+    fun day10_calculateScore_withInput() {
+        val day10 = Day10()
+        val result = day10.part1(day10.readInput(DAY10_INPUT))
+        assertEquals(370407, result)
+    }
+
+    /**
+     * The five lines' completion strings have total scores as follows:
+     *
+     *    }}]])})] - 288957 total points.
+     *    )}>]}) - 5566 total points.
+     *    }}>}>)))) - 1480781 total points.
+     *    ]]}}]}]}> - 995444 total points.
+     *    ])}> - 294 total points.
+     *    
+     *    Autocomplete tools are an odd bunch: the winner is found by sorting all of the scores and then taking the middle score. (There will always be an odd number of scores to consider.) In this example, the middle score is 288957 because there are the same number of scores smaller and larger than it.
+     */
+    @Test
+    fun day10_calculateMiddleScore() {
+        val day10 = Day10()
+        val result = day10.part2(day10.readInput(DAY10_INPUT_EXAMPLE))
+        assertEquals(288957, result)
+    }
+    
+    @Test
+    fun day10_calculateMiddleScore_withInput() {
+        val day10 = Day10()
+        val result = day10.part2(day10.readInput(DAY10_INPUT))
+        assertEquals(288957, result)
+    }
+}
