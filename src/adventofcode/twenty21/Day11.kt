3a26299e6e057063614df933ac97a89e6b056011
Martin Trollip <Martin.Trollip@hensoldt.net>
2021-12-11 18:01:17 +0200

    [adventofcode] 2021 Day 11 part 1 and 2
    This can be simplified a lot (even just by removing duplicates).
    
    Also want to see if a nice animation of this can be completed

--- src/adventofcode/twenty21/Day11.kt
+++ src/adventofcode/twenty21/Day11.kt
@@ -1,5 +1,6 @@
 package adventofcode.twenty21
 
+import java.awt.Point
 import java.io.File
 import java.util.*
 
@@ -11,30 +12,145 @@ private const val DAY11_INPUT = "src/res/twenty21/day11_input"
 
 fun main() {
     val day11 = Day11()
-//    println(
-//        "What is the total syntax error score for those errors?? ${
-//            day10.part1(day10.readInput(DAY10_INPUT))
-//        }"
-//    )
-//    println(
-//        "What is the middle score? ${
-//            day10.part2(day10.readInput(DAY10_INPUT))
-//        }"
-//    )
+    println(
+        "How many total flashes are there after 100 steps? ${
+            day11.part1(day11.readInput(DAY11_INPUT).toMutableMap(), 100)
+        }"
+    )
+    println(
+        "What is the first step during which all octopuses flash? ${
+            day11.part2(day11.readInput(DAY11_INPUT).toMutableMap())
+        }"
+    )
 }
 
 class Day11 {
 
-    fun readInput(fileName: String): List<String> {
-        return File(fileName).readLines()
+    fun readInput(fileName: String): Map<Point, Int> {
+        var row = 0
+        var col = 0
+        val map = mutableMapOf<Point, Int>()
+
+        File(fileName).readLines().forEach {
+            it.forEach {
+                map[Point(col, row)] = it.toString().toInt()
+                col++
+            }
+            row++
+            col = 0
+        }
+        return map
     }
 
-    fun part1(input: List<String>): Int {
-        return 1
+    fun part1(whales: MutableMap<Point, Int>, repeat: Int): Int {
+        var counts = 0
+
+        repeat(repeat) {
+            val flashes = emptyList<Point>().toMutableList()
+            
+            //First increase energy level of all whales by 1
+            whales.forEach {
+                whales[it.key] = it.value + 1
+            }
+
+            var delta = -1
+            while (delta != 0) {
+                val newFlashes = emptyList<Point>().toMutableList()
+                
+                //Let whales > 9 flash and go to 0
+                whales.forEach {
+                    if (it.value > 9 && !flashes.contains(it.key)) {
+                        newFlashes += it.key
+                    }
+                }
+
+                //Increase energy level of neighbors
+                val allNeighbours = allNeighbours(newFlashes)
+                allNeighbours.forEach {
+                    if (whales[it] != null) {
+                        whales[it] = whales[it]!! + 1
+                    }
+                }                
+                
+                delta = newFlashes.size
+                flashes.addAll(newFlashes)
+                newFlashes.clear()
+            }
+
+            //Then start from the top
+            
+            whales.filter { flashes.contains(it.key) }.forEach { t, u -> 
+                whales[t] = 0
+            }
+            
+            counts += flashes.size
+            flashes.clear()
+        }
+
+        return counts
+    }
+    
+    private fun allNeighbours(points: List<Point>) = points.flatMap {
+        listOf(
+            Point(it.x - 1, it.y - 1),
+            Point(it.x, it.y - 1),
+            Point(it.x + 1, it.y - 1),
+            Point(it.x - 1, it.y),
+            Point(it.x + 1, it.y),
+            Point(it.x - 1, it.y + 1),
+            Point(it.x, it.y + 1),
+            Point(it.x + 1, it.y + 1)
+        )
     }
 
-    fun part2(input: List<String>): Long {
-        return 2
+    fun part2(whales: MutableMap<Point, Int>): Int {
+        var steps = 0
+        var flashed = 0
+        
+        while (flashed < 100) {
+            val flashes = emptyList<Point>().toMutableList()
+
+            //First increase energy level of all whales by 1
+            whales.forEach {
+                whales[it.key] = it.value + 1
+            }
+
+            var delta = -1
+            while (delta != 0) {
+                val newFlashes = emptyList<Point>().toMutableList()
+
+                //Let whales > 9 flash and go to 0
+                whales.forEach {
+                    if (it.value > 9 && !flashes.contains(it.key)) {
+                        newFlashes += it.key
+                    }
+                }
+
+                //Increase energy level of neighbors
+                val allNeighbours = allNeighbours(newFlashes)
+                allNeighbours.forEach {
+                    if (whales[it] != null) {
+                        whales[it] = whales[it]!! + 1
+                    }
+                }
+
+                delta = newFlashes.size
+                flashes.addAll(newFlashes)
+                newFlashes.clear()
+            }
+
+            //Then start from the top
+
+            whales.filter { flashes.contains(it.key) }.forEach { t, u ->
+                whales[t] = 0
+            }
+
+            steps++
+            flashed = flashes.size
+            flashes.clear()
+        }
+
+        return steps
     }
 
 }
--- test/adventofcode/twenty21/Day11Test.kt
+++ test/adventofcode/twenty21/Day11Test.kt
@@ -9,13 +9,43 @@ import org.junit.Test
  */
 class Day11Test {
     private val DAY11_INPUT_EXAMPLE = "src/res/twenty21/day11_input_example"
+    private val DAY11_INPUT_EXAMPLE_SMALLER = "src/res/twenty21/day11_input_example_smaller"
     private val DAY11_INPUT = "src/res/twenty21/day11_input"
 
    
     @Test
-    fun day11_calculateScore_example() {
+    fun day11_countFlashes_example() {
         val day11 = Day11()
-        val result = day11.part1(day11.readInput(DAY11_INPUT_EXAMPLE))
-        assertEquals(26397, result)
+        val result = day11.part1(day11.readInput(DAY11_INPUT_EXAMPLE).toMutableMap(), 100)
+        assertEquals(1656, result)
     }
+    
+    @Test
+    fun day11_countFlashes_example_smaller() {
+        val day11 = Day11()
+        val result = day11.part1(day11.readInput(DAY11_INPUT_EXAMPLE_SMALLER).toMutableMap(), 100)
+        assertEquals(256, result)
+    }
+    
+    @Test
+    fun day11_countFlashes_withInput() {
+        val day11 = Day11()
+        val result = day11.part1(day11.readInput(DAY11_INPUT).toMutableMap(), 100)
+        assertEquals(2, result)
+    }
+
+    @Test
+    fun day11_countSteps_example() {
+        val day11 = Day11()
+        val result = day11.part2(day11.readInput(DAY11_INPUT_EXAMPLE).toMutableMap())
+        assertEquals(195, result)
+    }
+
+    @Test
+    fun day11_countSteps_withInput() {
+        val day11 = Day11()
+        val result = day11.part2(day11.readInput(DAY11_INPUT).toMutableMap())
+        assertEquals(195, result)
+    }
+    
 }
