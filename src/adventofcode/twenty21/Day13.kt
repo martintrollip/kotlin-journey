72b97730fde32cf834d0d9978687c6b76409a825
Martin Trollip <Martin.Trollip@hensoldt.net>
2021-12-13 08:47:25 +0200

    [adventofcode] 2021 Day 13 part 1 and 2

--- src/adventofcode/twenty21/Day13.kt
+++ src/adventofcode/twenty21/Day13.kt
@@ -1,7 +1,7 @@
 package adventofcode.twenty21
 
+import java.awt.Point
 import java.io.File
-import java.util.*
 
 /**
  * @author Martin Trollip
@@ -12,12 +12,12 @@ private const val DAY13_INPUT = "src/res/twenty21/day13_input"
 fun main() {
     val day13 = Day13()
     println(
-        "part1? ${
+        "How many dots are visible after completing just the first fold instruction on your transparent paper? ${
             day13.part1(day13.readInput(DAY13_INPUT))
         }"
     )
     println(
-        "part2? ${
+        "What code do you use to activate the infrared thermal imaging camera system? ${
             day13.part2(day13.readInput(DAY13_INPUT))
         }"
     )
@@ -25,16 +25,93 @@ fun main() {
 
 class Day13 {
 
-    fun readInput(fileName: String): List<String> {
-        return File(fileName).readLines()
+    fun readInput(fileName: String): Day13Input {
+        val points = mutableListOf<Point>()
+        val folds = mutableListOf<Point>()
+
+        File(fileName).readLines().forEach { line ->
+            if (line.contains(",")) {
+                val (x, y) = line.split(",")
+                points.add(Point(x.toInt(), y.toInt()))
+            }
+
+            if (line.contains("fold along ")) {
+                val (dir, value) = line.replace("fold along ", "").split("=")
+
+                when (dir) {
+                    "x" -> folds.add(Point(value.toInt(), 0))
+                    "y" -> folds.add(Point(0, value.toInt()))
+                }
+            }
+        }
+
+        return Day13Input(points, folds)
     }
 
-    fun part1(input: List<String>): Int {
-        return 1
+    fun part1(input: Day13Input): Int {
+        val fold = input.folds.first()
+        val points = input.points.map { it.reflect(fold) }
+
+        return points.distinct().count()
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: Day13Input): Int {
+        var points = input.points
+        
+        for (fold in input.folds) {
+            points = points.map { it.reflect(fold) }
+        }
+        
+        printPoints(points)
         return 2
     }
+
+    private fun printPoints(points: List<Point>) {
+        val minX = points.minByOrNull { it.x }!!.x
+        val maxX = points.maxByOrNull { it.x }!!.x
+        val minY = points.minByOrNull { it.y }!!.y
+        val maxY = points.maxByOrNull { it.y }!!.y
+
+        for (y in minY..maxY) {
+            for (x in minX..maxX) {
+                if (points.contains(Point(x, y))) {
+                    print("#")
+                } else {
+                    print(" ")
+                }
+            }
+            println()
+        }
+    }
+
+    /**
+     * Reflect a point around the given fold line
+     *
+     * Check if x or y?
+     *
+     * If above fold, don't change, if below fold, change
+     *
+     *  If below fold,  calculate distance from fold.
+     *    New coordinate is fold - that distance
+     */
+    private fun Point.reflect(fold: Point): Point {
+        var x = this.x
+        var y = this.y
+
+        if (fold.x == 0) {
+            y = if (fold.y < this.y) fold.y - (this.y - fold.y) else this.y
+        }
+
+        if (fold.y == 0) {
+            x = if (fold.x < this.x) fold.x - (this.x - fold.x) else this.x
+        }
+
+        return Point(x, y)
+    }
+
+    data class Day13Input(
+        val points: List<Point>,
+        val folds: List<Point>
+    )
 }
 
--- test/adventofcode/twenty21/Day13Test.kt
+++ test/adventofcode/twenty21/Day13Test.kt
@@ -10,5 +10,36 @@ import org.junit.Test
 class Day13Test {
     private val DAY13_INPUT_EXAMPLE = "src/res/twenty21/day13_input_example"
     private val DAY13_INPUT = "src/res/twenty21/day13_input"
-
+    
+    /**
+     * The transparent paper is pretty big, so for now, focus on just completing the first fold. After the first fold in the example above, 17 dots are visible - dots that end up overlapping after the fold is completed count as a single dot.
+     */
+    @Test
+    fun day11_foldFirst_example() {
+        val day13 = Day13()
+        val result = day13.part1(day13.readInput(DAY13_INPUT_EXAMPLE))
+        Assert.assertEquals(17, result)
+    }
+    
+    @Test
+    fun day11_foldFirst_withInput() {
+        val day13 = Day13()
+        val result = day13.part1(day13.readInput(DAY13_INPUT))
+        Assert.assertEquals(720, result)
+    }
+    
+    @Test
+    fun day11_foldAll_example() {
+        val day13 = Day13()
+        val result = day13.part2(day13.readInput(DAY13_INPUT_EXAMPLE))
+        Assert.assertEquals(2, result)
+    }
+    
+    @Test
+    fun day11_foldAll_withInput() {
+        val day13 = Day13()
+        val result = day13.part2(day13.readInput(DAY13_INPUT))
+        Assert.assertEquals(2, result)
+    }
+    
 }
